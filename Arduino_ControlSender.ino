c15b830d1947b38cefb933edc6150f647f3ec9b3
Ahmet YILDIRIM <yildirimcoder@gmail.com>
2017-04-23 23:33:11 +0200

    Update Arduino_ControlSender.ino

--- Arduino_ControlSender.ino
+++ Arduino_ControlSender.ino
@@ -1,17 +1,16 @@
 #include <SoftwareSerial.h>
 
 const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
-const int analogOutPin = 9; // Analog output pin that the LED is attached to
 
 int sensorValue = 0;        // value read from the pot
 int outputValue = 0;        // value output to the PWM (analog out)
 
-SoftwareSerial Bluetooth(2, 3);
+SoftwareSerial Bluetooth(8, 9);
 
 void setup() {
   // initialize serial communications at 9600 bps:
   Bluetooth.begin(115200);
-  Serial.begin(115200);
+  //Serial.begin(115200);
 }
 
 void loop() {
@@ -21,7 +20,7 @@ void loop() {
   outputValue = map(sensorValue, 0, 1023, 0, 255);
 
   Bluetooth.write(outputValue);
-  Serial.println(outputValue);
+  //Serial.println(outputValue);
 
   // wait 2 milliseconds before the next loop
   // for the analog-to-digital converter to settle

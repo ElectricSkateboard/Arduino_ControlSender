92ba1b4a8dfaf3dd421f9c1aa8f6a7a2e60a3715
Ahmet YILDIRIM <yildirimcoder@gmail.com>
2017-04-24 14:45:18 +0200

    Update Arduino_ControlSender.ino

--- Arduino_ControlSender.ino
+++ Arduino_ControlSender.ino
@@ -4,6 +4,7 @@ const int analogInPin = A0;  // Analog input pin that the potentiometer is attac
 
 int sensorValue = 0;        // value read from the pot
 int outputValue = 0;        // value output to the PWM (analog out)
+int lastval = 0;
 
 SoftwareSerial Bluetooth(8, 9);
 
@@ -18,10 +19,13 @@ void loop() {
   sensorValue = analogRead(analogInPin);
   // map it to the range of the analog out:
   outputValue = map(sensorValue, 0, 1023, 0, 255);
+  if(outputValue != lastval)
+  {
+    Bluetooth.write(outputValue);
+    //Serial.println(outputValue);
 
-  Bluetooth.write(outputValue);
-  //Serial.println(outputValue);
-
+    lastval = outputValue;
+  }
   // wait 2 milliseconds before the next loop
   // for the analog-to-digital converter to settle
   // after the last reading:

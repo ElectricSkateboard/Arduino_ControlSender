c243056b57b2976671aef2ffe602289b00e60da2
Ahmet YILDIRIM <yildirimcoder@gmail.com>
2017-05-16 22:59:46 +0200

    Update Arduino_ControlSender.ino

--- Arduino_ControlSender.ino
+++ Arduino_ControlSender.ino
@@ -6,6 +6,8 @@ int sensorValue = 0;        // value read from the pot
 int outputValue = 0;        // value output to the PWM (analog out)
 int lastval = 0;
 
+int trustCounter = 0;
+
 SoftwareSerial Bluetooth(8, 9);
 
 void setup() {
@@ -18,16 +20,23 @@ void loop() {
   // read the analog in value:
   sensorValue = analogRead(analogInPin);
   // map it to the range of the analog out:
-  outputValue = map(sensorValue, 0, 1023, 0, 255);
+  outputValue = map(sensorValue, 0, 1450, 0, 255);
   if(outputValue != lastval)
   {
-    Bluetooth.write(outputValue);
-    //Serial.println(outputValue);
-
-    lastval = outputValue;
+    if(lastval - outputValue > 30 && trustCounter < 4)
+    {
+      trustCounter++;
+    }
+    else
+    {
+      Bluetooth.write(outputValue);
+      //Serial.println(outputValue);
+      lastval = outputValue;
+      trustCounter = 0;
+    }
   }
   // wait 2 milliseconds before the next loop
   // for the analog-to-digital converter to settle
   // after the last reading:
-  delay(5);
+  delay(2);
 }
